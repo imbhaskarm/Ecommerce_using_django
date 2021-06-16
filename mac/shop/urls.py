383faa76697b5e1018d7034c7a608214c557f531
imbhaskarm <bhaskarmaheshwari27gmail.com>
2021-06-16 22:24:04 +0530

    first commit

---
+++ mac/shop/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+     path("",views.index,name="ShopHome"),
+     path("about/",views.about, name="AboutUs"),
+     path("contact/", views.contact, name="ContactUs"),
+     path("tracker/", views.tracker, name="TrackingStatus"),
+     path("search/", views.search, name="Search"),
+     path("productview/<int:myid>", views.productview, name="productview"),
+     path("checkout/", views.checkout, name="checkout"),
+
+
+]
